5f4297a73cfa706b3f6823787726cc3fa4ec032d
Luciano Lopez <lucianlopez@frba.utn.edu.ar>
2021-12-09 19:01:28 -0300

    cambios consulta

--- Trabajo practico/consulta_estadistica.sql
+++ Trabajo practico/consulta_estadistica.sql
@@ -1,4 +1,4 @@
 USE `CONTROL_ACCESO`;
 SELECT * FROM Puerta_estadistica
 GROUP BY Puerta_estadistica.ID
-ORDER BY COUNT(Puerta_estadistica.Denegados) ASC;
+ORDER BY COUNT(Puerta_estadistica.ID) DESC;
